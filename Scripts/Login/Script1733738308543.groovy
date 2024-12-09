9441fc0687e82928edcb06c8211579c4904b3973
JayJay <jayjay.olpos@supportcommunity.com>
2024-12-09 22:31:24 +0800

    Update signin. added signout

--- Scripts/Login/Script1733738308543.groovy
+++ Scripts/Login/Script1733738308543.groovy
@@ -27,8 +27,10 @@ WebUI.click(findTestObject('Object Repository/Login OR/Page_CURA Healthcare Serv
 
 WebUI.setText(findTestObject('Object Repository/Login OR/Page_CURA Healthcare Service/input_Username_username'), 'John Doe')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Login OR/Page_CURA Healthcare Service/input_Password_password'),
-	'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
+WebUI.setEncryptedText(findTestObject('Object Repository/Login OR/Page_CURA Healthcare Service/input_Password_password'), 
+    'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
 
 WebUI.click(findTestObject('Object Repository/Login OR/Page_CURA Healthcare Service/button_Login'))
+
+WebUI.closeBrowser()
 
